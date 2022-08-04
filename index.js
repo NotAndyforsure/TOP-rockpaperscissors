57441e07b5d89ad85969e7eb92ac63211c336f07
Andy <agcmall05@gmail.com>
2022-08-04 16:57:53 +0100

    Execution stage finished
    the game perfectly runs on the console although i did not add a response to typos, but anyways i'll add it further down the line when i create the gui.
    
    :
    diff --git c/index.js i/index.js
    index 85d6611..10acfe9 100644
    --- c/index.js
    +++ i/index.js
    @@ -4,16 +4,50 @@ function getComputerChoice(){
    
       switch(generator){
         case 0:
    -      return "Rock!"
    +      return "ROCK!"
           break;
           case 1:
    -        return "Paper!"
    +        return "PAPER!"
             break;
             case 2:
    -          return "Scissor!"
    +          return "SCISSOR!"
               break;
    
       }
    
     }
    -console.log(getComputerChoice())
    +
    +function playerSelection(select){
    +
    +  let choice=select.toUpperCase()
    +let respc =getComputerChoice()
    +console.log("You: " + choice)
    +
    +if(respc===select){
    +  return "It's a tie"
    +}else if(respc=== "ROCK!" && choice==="PAPER"){
    +  console.log("PC: "+respc)
    +  return "You win!!! Paper beats rock"
    +}else if(respc === "ROCK!" && respc==="SCISSOR"){
    +  console.log("PC:" + respc)
    +return "You lose!!! Rock beats Scissors"
    +}else if(respc ==="PAPER!" && choice ==="ROCK"){
    +  console.log("PC:" + respc)
    +  return "You lose!! Paper beats rock"
    +}else if(respc==="PAPER!" && choice==="SCISSOR"){
    +  console.log("PC:" + respc)
    +  return "you win!! Scissor beats paper"
    +}else if(respc=="SCISSOR!" && choice==="ROCK"){
    +  console.log("PC: " +respc)
    +  return "You win!! Rock beats scissor"
    +}else if(respc==="SCISSOR!" && choice==="PAPER"){
    +  console.log("PC: " + respc)
    +  return "You lose!! Scissor beats paper anyday"
    +
    +}else{
    +  console.log("PC: " + respc)
    +  return "I'ts a tie!!!!!"
    +}
    +}
    +
    +

--- index.js
+++ index.js
@@ -4,16 +4,50 @@ function getComputerChoice(){
   
   switch(generator){
     case 0:
-      return "Rock!"
+      return "ROCK!"
       break;
       case 1:
-        return "Paper!"
+        return "PAPER!"
         break;
         case 2:
-          return "Scissor!"
+          return "SCISSOR!"
           break;
 
   }
   
 }
-console.log(getComputerChoice())
+
+function playerSelection(select){
+
+  let choice=select.toUpperCase()
+let respc =getComputerChoice()
+console.log("You: " + choice)
+
+if(respc===select){
+  return "It's a tie"
+}else if(respc=== "ROCK!" && choice==="PAPER"){
+  console.log("PC: "+respc)
+  return "You win!!! Paper beats rock"
+}else if(respc === "ROCK!" && respc==="SCISSOR"){
+  console.log("PC:" + respc)
+return "You lose!!! Rock beats Scissors"
+}else if(respc ==="PAPER!" && choice ==="ROCK"){
+  console.log("PC:" + respc)
+  return "You lose!! Paper beats rock"
+}else if(respc==="PAPER!" && choice==="SCISSOR"){
+  console.log("PC:" + respc)
+  return "you win!! Scissor beats paper"
+}else if(respc=="SCISSOR!" && choice==="ROCK"){
+  console.log("PC: " +respc)
+  return "You win!! Rock beats scissor"
+}else if(respc==="SCISSOR!" && choice==="PAPER"){
+  console.log("PC: " + respc)
+  return "You lose!! Scissor beats paper anyday"
+
+}else{
+  console.log("PC: " + respc)
+  return "I'ts a tie!!!!!"
+}
+}
+
+
