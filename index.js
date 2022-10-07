e850596c23bebeed1609aad48bcd542127468afe
Andy <agcmall05@gmail.com>
2022-10-07 21:34:39 +0100

    Created the engines(functions) necessary to run the game, but still haven't completed the game console project

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 function getComputerChoice(){
 
   const generator =Math.floor(Math.random()*3);
@@ -17,37 +22,71 @@ function getComputerChoice(){
   
 }
 
-function playerSelection(select){
 
+function playerSelection(select){
+let computer=0
+let user=0
   let choice=select.toUpperCase()
 let respc =getComputerChoice()
 console.log("You: " + choice)
 
-if(respc===select){
-  return "It's a tie"
-}else if(respc=== "ROCK!" && choice==="PAPER"){
+
+if(respc=== "ROCK!" && choice==="PAPER"){
+  
   console.log("PC: "+respc)
+ 
   return "You win!!! Paper beats rock"
+
 }else if(respc === "ROCK!" && respc==="SCISSOR"){
+  
   console.log("PC:" + respc)
+ 
 return "You lose!!! Rock beats Scissors"
+
 }else if(respc ==="PAPER!" && choice ==="ROCK"){
+  
   console.log("PC:" + respc)
+  
   return "You lose!! Paper beats rock"
+
 }else if(respc==="PAPER!" && choice==="SCISSOR"){
+ 
   console.log("PC:" + respc)
+  
   return "you win!! Scissor beats paper"
+
 }else if(respc=="SCISSOR!" && choice==="ROCK"){
+ 
   console.log("PC: " +respc)
   return "You win!! Rock beats scissor"
+
 }else if(respc==="SCISSOR!" && choice==="PAPER"){
+  
   console.log("PC: " + respc)
+ 
   return "You lose!! Scissor beats paper anyday"
 
 }else{
   console.log("PC: " + respc)
   return "I'ts a tie!!!!!"
 }
+
 }
+
+
+
+function game(){
+ 
+
+for(let i= 0; i < 5; i++){
+
+let select=prompt("Rock Paper or scissor")
+playerSelection(select)
+
+}
+
+}
+
+
 
 
