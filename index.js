8cd6d424fcbefebf61fdc4798a8524e1a17d47e1
Andy <agcmall05@gmail.com>
2022-10-15 15:44:30 +0100

    final app after merging problem
    1-ui created with flexbox
    2-play enabled if conditions satisfy
    3-replay function added
    4-show choice in dom added
    5-show pointes in dom added
    6-show name added
    7-sound effects added
    8-anounce winner function added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
-//tally variables
+//tally variables------------------------------------------------------------------------------------
 
-var computer=0
-var user= 0
+var computer=0;
+var user= 0;
 
 
 
-//Computer rock paper scissor generator
+
+
+//Computer choice generator--------------------------------------------------------------------------------
 
 function getComputerChoice(){
 
@@ -30,87 +32,416 @@ function getComputerChoice(){
 
 
 
-//win or lose conditions
+
+
+
+
+
+
+
+
+
+
+//win or lose conditions----------------------------------------------------------------------------------------------
 
 function playerSelection(select){
 
+  
+
   let choice=select.toUpperCase()
-let respc =getComputerChoice()
+  let respc =getComputerChoice()
+  let userScore=document.querySelector(".userscore")
+  let pcScore=document.querySelector(".pcscore")
 
-console.log("You: " + choice)
-console.log(`computer: ${respc}`)
 
 
-if(respc=== "ROCK" && choice==="PAPER"){
-  
-  user++
+
+  showChoice(choice,respc)
+
+
+
+  if(respc=== "ROCK" && choice==="PAPER"){
+
+
+    user++
+    userScore.textContent=user
+    pcScore.textContent=computer
+    winnerFinder()
+
  
-}else if(respc === "ROCK" && choice==="SCISSOR"){
-  
- computer++
+  }else if(respc === "ROCK" && choice==="SCISSOR"){
 
-}else if(respc ==="PAPER" && choice ==="ROCK"){
   
-computer++
+    computer++
+    userScore.textContent=user
+    pcScore.textContent=computer
+    winnerFinder()
 
-}else if(respc==="PAPER" && choice==="SCISSOR"){
+
+
+  }else if(respc ==="PAPER" && choice ==="ROCK"){
+  
+    computer++
+    userScore.textContent=user
+    pcScore.textContent=computer
+    winnerFinder()
+
+
+  }else if(respc==="PAPER" && choice==="SCISSOR"){
  
   
-  user++
+    user++
+    userScore.textContent=user
+    pcScore.textContent=computer
+    winnerFinder()
  
 
-}else if(respc=="SCISSOR" && choice==="ROCK"){
+  }else if(respc=="SCISSOR" && choice==="ROCK"){
+
  
-  user++
+   user++
+   userScore.textContent=user
+   pcScore.textContent=computer
+   winnerFinder()
 
-}else if(respc==="SCISSOR" && choice==="PAPER"){
+
+  }else if(respc==="SCISSOR" && choice==="PAPER"){
   
- computer++
+    computer++
+    userScore.textContent=user
+    pcScore.textContent=computer
+    winnerFinder()
 
-}else if(respc===choice){
 
-  user++
+  }else if(respc===choice){
 
-  computer++
+    user++
+    computer++
+    userScore.textContent=user
+    pcScore.textContent=computer
+    winnerFinder()
   
 
-}else{
+  }else{
 
-  user--
+    user--
+    userScore.textContent=user
+    pcScore.textContent=computer
 
-  console.log("Play wrong, lose a point!")
+    console.log("Play wrong, lose a point!")
+
+  }
 
 }
 
-}
 
 
-//game starter function
+
+
+
+
+
+
+
+
+
+
+//game engine----------------------------------------------------------------------
 
 function game(){
- 
+
+  findName()
+
 computer= 0
 user= 0
-for(let i= 0; i < 5; i++){
 
-let select=prompt("Rock Paper or scissor")
-playerSelection(select)
 
-}
 
-console.log(winner(computer,user))
 }
 
 
 
-//winner finder function
+
+
+
+
+
+
+
+//winner finder --------------------------------------------------------------------------------------------------------------
 function winner(a,b){
+  let btn=document.querySelector(".btn")
+  let anounce=document.querySelector(".body>h1")
+  let userImg=document.querySelector(".userimg>img")
+  let pcImg=document.querySelector(".pcimg>img")
+  let win=document.querySelector(".win")
+  let lost=document.querySelector(".lost")
 
-return a === b? ` That's tie!!!!!!!!!!!!. to play again write game()`:a>b? ` Computer:${a}   You: ${b}. You LOSE!!!!!!!!!`:` Computer:${a}  You: ${b}. You SAVED THE WORLD CHOSEN ONE!!!!!!!!`
+  if(a>b){
+    lost.play()
+    anounce.textContent="You lost!!!!"
+    userImg.setAttribute("src","./images/default.jpg")
+    pcImg.setAttribute("src","./images/default.jpg")
+    btn.innerHTML=`<button class="btnreplay">Play()</button> `
+    let btnReplay=document.querySelector(".btnreplay")
+    btnReplay.addEventListener("click",gameStart)
+   
+
+  }else if(a<b){
+    win.play()
+    anounce.textContent="You win!!!!"
+    userImg.setAttribute("src","./images/default.jpg")
+    pcImg.setAttribute("src","./images/default.jpg")
+    btn.innerHTML=`<button class="btnreplay">Play()</button> `
+    let btnReplay=document.querySelector(".btnreplay")
+    btnReplay.addEventListener("click",gameStart)
+    btnPlay.addEventListener("click",gameStart)
+
+  }
+  
+}
+
+
+// winner finder condition-----------------------------------------------------------------------------------------------------------------
+function winnerFinder(){
+
+if(user >=5 || computer >= 5){
+    
+
+  winner(computer,user)
+}
 
 }
 
-game()
+
+
+
+
+
+
+
+
+
+
+// game starter function-----------------------------------------------
+
+let btnPlay=document.querySelector(".btnplay")
+
+btnPlay.addEventListener("click",gameStart)
+
+
+
+
+
+
+function gameStart(){
+  let anounce=document.querySelector(".body>h1")
+ 
+  anounce.textContent="RocK Paper Scissors Edition:"
+
+  computer=0
+  user=0
+  let userScore=document.querySelector(".userscore")
+  let pcScore=document.querySelector(".pcscore")
+  userScore.textContent=user
+  pcScore.textContent=computer
+
+  findName()
+
+  let btn=document.querySelector(".btn")
+
+  
+
+
+
+
+  btn.innerHTML=` 
+                  <button class="btnrock">Rock</button> 
+                  <button class="btnpaper">Paper</button>
+                  <button class="btnscissor">Scissor</button>
+
+                 `
+
+
+
+
+  
+
+  
+
+
+  let btnRock=document.querySelector(".btnrock")
+  let btnPaper=document.querySelector(".btnpaper")
+  let btnScissor=document.querySelector(".btnscissor")
+
+
+  btnRock.addEventListener("click",function(e){
+ 
+    e.stopPropagation()
+
+    playerSelection("ROCK")
+
+
+  })
+
+
+
+
+  btnPaper.addEventListener("click",function(e){
+
+    e.stopPropagation()
+
+    playerSelection("paper")
+
+
+  })
+
+
+
+
+  btnScissor.addEventListener("click",function(e){
+
+
+    e.stopPropagation()
+
+    playerSelection("scissor")
+
+  })
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// name finder-------------------------------------------------------------------------
+
+
+function findName(){
+
+  let name=prompt("Please enter your name here:")
+  let userh=document.querySelector(".userh")
+  userh.textContent=name
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Choice shower-------------------------------------------------------------------------------------------
+function showChoice(choice,respc){
+
+  let userImg=document.querySelector(".userimg img");
+  let pcImg=document.querySelector(".pcimg img");
+
+  switch(choice){
+    
+    
+
+      case "ROCK":
+      userImg.setAttribute("src","./images/rock.jpg")
+      break;
+
+      case "PAPER":
+      userImg.setAttribute("src","./images/paper.jpg")
+      break;
+
+      case "SCISSOR":
+      userImg.setAttribute("src","./images/scissor.jpg")
+      break;
+
+
+
+
+  }
+
+
+
+  switch(respc){
+
+    case "ROCK":
+      pcImg.setAttribute("src","./images/rock.jpg")
+      break;
+
+      case "PAPER":
+      pcImg.setAttribute("src","./images/paper.jpg")
+      break;
+
+      case "SCISSOR":
+      pcImg.setAttribute("src","./images/scissor.jpg")
+      break;
+
+
+
+
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
